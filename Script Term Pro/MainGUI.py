eae677c7f989f17c5817f40ba037661c432cb1d7
niche0905 <yudle537@gmail.com>
2024-05-24 23:48:34 +0900

    캐릭터 정보 페이지 하단부
    하단부의 능력치 부분 및 입력 정보들을 임시 값으로 채우고 배치해 보았다.

--- Script Term Pro/MainGUI.py
+++ Script Term Pro/MainGUI.py
@@ -12,6 +12,8 @@ class MainGUI:
 
         self.font = font.Font(self.window, size=16, weight='bold', family='굴림')
         self.fontB = font.Font(self.window, size=16, weight='bold', family='arial')
+        self.fontT = font.Font(self.window, size=24, weight='bold', family='굴림')
+        self.fontV = font.Font(self.window, size=28, weight='bold', family='굴림')
 
         image = Image.open('Resource/Image/tempCharImage.png')
         image = image.resize((200, 200))
@@ -37,9 +39,11 @@ class MainGUI:
         frame1_2 = Frame(frame1, width=600, height=700, bg='gold')
         frame1_2.pack()
 
+        # 종합정보
         Frame(frame1_2, width=600, height=30, bg='LightBlue1').pack()
         frame1_2_1 = Frame(frame1_2, width=600, height=200, bg='gray50')
         frame1_2_1.pack()
+        # 이미지 액자
         frame1_2_1_1 = Frame(frame1_2_1, width=160, height=200, bg='#ede0c8')
         frame1_2_1_1.place(x=50, y=0)
         frame1_2_1_1_1 = Frame(frame1_2_1_1, width=140, height=180)
@@ -48,6 +52,7 @@ class MainGUI:
         self.charImageLabel = Label(frame1_2_1_1_1, text='이미지', image=self.testImage, width=140, height=180, padx=10, pady=10)
         self.charImageLabel.image = self.testImage
         self.charImageLabel.pack(expand=True)
+        # 정보
         frame1_2_1_2 = Frame(frame1_2_1, width=290, height=200, bg='purple1')
         frame1_2_1_2.place(x=270, y=0)
         name = '김땡땡'    # 테스트용
@@ -69,24 +74,49 @@ class MainGUI:
         Frame(frame1_2, width=600, height=30, bg='LightBlue1').pack()
         frame1_2_2 = Frame(frame1_2, width=600, height=300, bg='gray50')
         frame1_2_2.pack()
+        # 능력치
         frame1_2_2_1 = Frame(frame1_2_2, width=240, height=300, bg='khaki1')
         frame1_2_2_1.pack_propagate(False)
         frame1_2_2_1.place(x=40, y=0)
         frame1_2_2_1_1 = Frame(frame1_2_2_1, width=200, height=40, bg='sky blue')
+        frame1_2_2_1_1.pack_propagate(False)
         frame1_2_2_1_1.pack()
-        frame1_2_2_1_2 = Frame(frame1_2_2_1, width=240, height=260, bg='thistle')
-        frame1_2_2_1_2.pack()
+        self.statusLabel = Label(frame1_2_2_1_1, text='능력치', font=self.fontT)
+        self.statusLabel.pack(side=BOTTOM)
+        frame1_2_2_1_2 = Frame(frame1_2_2_1, width=140, height=260, bg='thistle', padx=10, pady=10)
+        frame1_2_2_1_2.pack_propagate(False)
+        frame1_2_2_1_2.pack(padx=5, pady=5)
+        self.HPLabel = Label(frame1_2_2_1_2, text='HP: '+str(50000), font=self.font)
+        self.HPLabel.pack(side=TOP, anchor=W, expand=Y)
+        self.MPLabel = Label(frame1_2_2_1_2, text='MP: '+str(10000), font=self.font)
+        self.MPLabel.pack(side=TOP, anchor=W, expand=Y)
+        self.STRLabel = Label(frame1_2_2_1_2, text='STR: '+str(1000), font=self.font)
+        self.STRLabel.pack(side=TOP, anchor=W, expand=Y)
+        self.DEXLabel = Label(frame1_2_2_1_2, text='DEX: '+str(500), font=self.font)
+        self.DEXLabel.pack(side=TOP, anchor=W, expand=Y)
+        self.INTLabel = Label(frame1_2_2_1_2, text='INT: '+str(100), font=self.font)
+        self.INTLabel.pack(side=TOP, anchor=W, expand=Y)
+        self.LUKLabel = Label(frame1_2_2_1_2, text='LUK: '+str(150), font=self.font)
+        self.LUKLabel.pack(side=TOP, anchor=W, expand=Y)
+        # 장비
         frame1_2_2_2 = Frame(frame1_2_2, width=240, height=300, bg='burlywood1')
         frame1_2_2_2.pack_propagate(False)
         frame1_2_2_2.place(x=320, y=0)
         frame1_2_2_2_1 = Frame(frame1_2_2_2, width=200, height=40, bg='sky blue')
+        frame1_2_2_2_1.pack_propagate(False)
         frame1_2_2_2_1.pack()
-        frame1_2_2_2_2 = Frame(frame1_2_2_2, width=240, height=260, bg='thistle')
-        frame1_2_2_2_2.pack()
+        self.equipmnLabel = Label(frame1_2_2_2_1, text='장비', font=self.fontT)
+        self.equipmnLabel.pack(side=BOTTOM)
+        frame1_2_2_2_2 = Frame(frame1_2_2_2, width=240, height=260, bg='thistle', padx=10, pady=10)
+        frame1_2_2_2_2.pack_propagate(False)
+        frame1_2_2_2_2.pack(padx=5, pady=5)
 
         Frame(frame1_2, width=600, height=30, bg='LightBlue1').pack()
         frame1_2_3 = Frame(frame1_2, width=600, height=80, bg='gray50')
+        frame1_2_3.pack_propagate(False)
         frame1_2_3.pack()
+        self.mureungLabel = Label(frame1_2_3, text='무릉 '+str(49)+'층 '+str(12)+':'+str(59), font=self.fontV)
+        self.mureungLabel.pack(side=TOP)
 
         #
         #
