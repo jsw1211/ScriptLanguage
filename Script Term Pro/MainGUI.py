b39c15db50ee2ad53f94dfd27cf289c022ec9012
jsw1211 <jswtiger@naver.com>
2024-05-30 01:41:56 +0900

    무기 레벨에 따라 필요한 메소가 달라지므로 무기 레벨 넣을 수 있는 텍스트 구현 및 메소 계산기

--- Script Term Pro/MainGUI.py
+++ Script Term Pro/MainGUI.py
@@ -259,6 +259,8 @@ class MainGUI:
         # Label(frame5, text='검키우기', fg='purple', font='helvetica 48').pack()
         frame5_1 = Frame(frame5, width=600, height=100, background='snow2')
         frame5_1.pack()
+        self.searchStr_2 = StringVar()
+        Entry(frame5_1, textvariable=self.searchStr_2, width=20, justify=LEFT, font=self.font).place(x=50, y=0)
         self.spentMesoLabel = Label(frame5_1, text='총 사용한 메소: ~~', width=20, height=2, font=self.fontB, background='slate blue')
         self.spentMesoLabel.place(x=100, y=40, width=400, height=40)
         frame5_2 = Frame(frame5, width=600, height=400, background='deep sky blue')
@@ -633,6 +635,25 @@ class MainGUI:
         self.succesPercentLabel['text'] = str(float(succesPercent))+'%'
         self.failPercentLabel['text'] = str(float(failPercent))+'%'
         self.destroyPercentLabel['text'] = str(float(brokePercent))+'%'
+
+    def need_meso(self,s):
+        # if 0 <= s <= 9:
+        #     return 1000+((착용 무기 레벨)*(s+1)/36)
+        # elif s == 10:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/571)
+        # elif s == 11:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/314)
+        # elif s == 12:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/214)
+        # elif s == 13:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/157)
+        # elif s == 14:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/107)
+        # elif 15 <= s <= 24:
+        #     return 1000+((착용 무기 레벨)*((s+1)**2.7)/200)
+        # else:
+        #     pass
+        pass
 
 
 class ServerMod(Enum):
