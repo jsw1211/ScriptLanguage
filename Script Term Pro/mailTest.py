22798630911291d997fc12c37f3fc5b4b2596beb
niche0905 <yudle537@gmail.com>
2024-06-01 22:09:22 +0900

    메일에 이미지를 첨가하기
    메일 내용에 이미지를 첨가하여 보내기를 위한 코드 수정

--- Script Term Pro/mailTest.py
+++ Script Term Pro/mailTest.py
@@ -1,7 +1,11 @@
 import mimetypes
 import smtplib
-from email.mime.multipart  import MIMEMultipart
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 smtpServer = "smtp.gmail.com"
 mailHost = "msTermProject2024@gmail.com"
@@ -14,8 +18,25 @@ msg['From'] = mailHost
 msg['To'] = sendMailAddress
 msg['Subject'] = "Test Mail"
 
-msgPart = MIMEText('123', 'plain')
+mainText = '123456'
+htmlBody = f"""
+    <html>
+    <body>
+    <p>{mainText}</p>
+    <img src="cid:image1">
+    </body>
+    </html>
+    """
+
+msgPart = MIMEText(htmlBody, 'html')
 msg.attach(msgPart)
+
+imgPath = 'Resource/Image/tempCharImage.png'
+with open(imgPath, 'rb') as imgFile:
+    imgData = imgFile.read()
+    img = MIMEImage(imgData)
+    img.add_header('Content-ID', '<image1>')
+    msg.attach(img)
 
 try:
     server = smtplib.SMTP(smtpServer, portNum)
@@ -25,5 +46,7 @@ try:
     server.sendmail(mailHost, sendMailAddress, text)
     server.quit()
     print('이메일 보내기 성공적')
+except smtplib.SMTPException as e:
+    print(f"이메일 보내기 실패. SMTPException: {e}")
 except Exception as e:
     print(f'이메일 보내기 실패. Error: {e}')
