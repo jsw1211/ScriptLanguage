1a55678132b118d28866277cb32848fcf858cbff
niche0905 <yudle537@gmail.com>
2024-06-02 16:27:49 +0900

    메일 테마 변경
    간단하게 메일 창 테마와 폰트를 변경하였다

--- Script Term Pro/mailGUI.py
+++ Script Term Pro/mailGUI.py
@@ -29,12 +29,14 @@ class mailGUI:
         self.window.protocol("WM_DELETE_WINDOW", self.onClose)
 
         self.mailFont = font.Font(self.window, size=16, weight='bold', family='굴림')
-        self.mailFontB = font.Font(self.window, size=32, weight='bold', family='굴림')
+        self.mailFontB = font.Font(self.window, size=32, weight='bold', family='메이플스토리')
 
+        frame = Frame(self.window, width=400, height=200, bg='orange')
+        frame.pack()
         self.mailStr = StringVar()
         entry = Entry(self.window, textvariable=self.mailStr, font=self.mailFont)
         entry.place(x=50, y=50, width=300, height=25)
-        button = Button(self.window, text='보내기', command=self.pressedSend, font=self.mailFontB)
+        button = Button(self.window, text='보내기', command=self.pressedSend, font=self.mailFontB, bg='SkyBlue1')
         button.place(x=100, y=100, width=200, height=80)
 
         self.window.mainloop()
